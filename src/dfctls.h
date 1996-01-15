edcea30c4adf5857a53ebf647b52e6651fcdf2c7
Ralf Hoppe <ralf.hoppe@ieee.org>
1996-01-15 07:49:26 +0000

    Import of module <dfcgen>

---
+++ src/dfctls.h
@@ -0,0 +1,17 @@
+#ifndef __DFCTLS_H
+
+#include "FDGEN.H"       /* generating control */
+
+
+void InitDfcgenDlgControls(HINSTANCE h);
+LRESULT CALLBACK StatusWndProc(HWND hwndDesk, UINT msg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK IncDecEditWndProc(HWND hEdit, UINT msg, WPARAM wParam, LPARAM lParam);
+
+/* additional notification codes for incremental edit fields
+ * defines with respect to windows edit field notify codes
+ */
+#define EN_MOUSE_INC    0x1000
+#define EN_MOUSE_DEC    0x1001
+
+#define __DFCTLS_H
+#endif
