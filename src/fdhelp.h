b6bcbade002d333a6b4e0b0b452d2ff280ede3cd
Ralf Hoppe <ralf.hoppe@ieee.org>
1997-12-23 17:52:22 +0000

    Import of module <dfcgen>

---
+++ src/fdhelp.h
@@ -0,0 +1,41 @@
+#define HelpContents            1
+
+#define HelpDefLinFirDlg        10
+#define HelpDefStdIirDlg        11
+#define HelpPredefSysDlg        12
+#define HelpDefSpecialDlg       13
+#define HelpWindowFuncDlg       14
+#define HelpFrequTransfDlg      15
+#define HelpFileDlg             16
+#define HelpProjectInfoDlg      17
+#define HelpEditCommentsDlg     18
+
+#define HelpCoeffsDlg           19
+#define HelpCoeffsDlgDenList    20
+#define HelpCoeffsDlgNumList    21
+#define HelpCoeffsDlgBtnModify  22
+#define HelpCoeffsDlgBtnErase   23
+#define HelpCoeffsDlgBtnRound   24
+#define HelpCoeffsDlgBtnFactor  25
+#define HelpCoeffsDlgBtnHelp    26
+#define HelpCoeffsDlgBtnNorm    27
+#define HelpCoeffsDlgTransferDC 28
+
+#define HelpOptDesktopDlg       29
+#define HelpOptMathDlg          30
+#define HelpOptAxisX            31
+#define HelpOptAxisY            32
+#define HelpOptMathFunc         33
+
+#define HelpRootsDlg            34
+#define HelpRootsDlgZeroList    35
+#define HelpRootsDlgPoleList    36
+#define HelpRootsDlgBtnCalcNew  37
+#define HelpRootsDlgBtnHelp     38
+#define HelpRootsDlgStability   39
+#define HelpRootsDlgMapPN       40
+
+#define HelpAboutDlg            41
+#define HelpLicenseDlg          42
+#define HelpPrintDlg            43
+
