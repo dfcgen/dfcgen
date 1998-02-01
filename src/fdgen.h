6bdb156db8b09e8fc0eee2d33307bc6ece6e6187
Ralf Hoppe <ralf.hoppe@ieee.org>
1998-02-01 15:07:50 +0000

    Import of module <dfcgen>

---
+++ src/fdgen.h
@@ -0,0 +1,15 @@
+#ifndef __FDGEN_H
+
+#define WINVER                 0x030A
+#define DEBUG                  FALSE
+#define LIB   /* version management static functions (no DLL) -> see VER.H */
+#define STRICT
+#define GENBORSTYLE            TRUE  /* generate with Borland style dialog */
+#define GENFPEOPT              FALSE    /* user defined FPE error handling */
+
+#include <WINDOWSX.H>   /* includes #define <windows.h> + message crackers */
+#include <VER.H>
+
+
+#define __FDGEN_H
+#endif
